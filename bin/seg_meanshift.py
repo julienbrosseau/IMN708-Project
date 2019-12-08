6b1e3f6f851c00e33ef582a63496604a286126c2
julienbrosseau <jbrosseau85@gmail.com>
2019-12-08 17:51:05 -0500

    MàJ implementation de la classe "Mean Shift clustering"

---
+++ bin/seg_meanshift.py
@@ -0,0 +1,42 @@
+# Segmentation des images par Mean Shift
+
+from sklearn.cluster import MeanShift
+import numpy as np
+
+import bin.treatment as tr
+treatment = tr.Treatment()
+
+class SegMeanShift():
+
+    def __init__(self):
+        # Initialisation de la classe
+        self.ms = MeanShift(
+            bandwidth = 50,
+            bin_seeding = True
+            )
+        self.labels = []
+
+    def fit(self, flat_img):
+        # Entrainement du modèle
+        return self.ms.fit(flat_img)
+    
+    def get_labels(self):
+        # Retourne les labels du modèle
+        return self.ms.labels_
+
+    def get_img_reshape(self, cropx, cropy):
+        # Retourne l'image redimensionnee
+        return np.reshape(self.labels, [cropx, cropy])
+    
+    def get_img_denoise(self, cropx, cropy):
+        # Retourne l'image debruitee
+        return treatment.median_filter(self.get_img_reshape(cropx, cropy), 5)
+
+    def set_labels(self, labels):
+        # Fixe les labels
+        self.labels = labels
+    
+    def set_clustering(self):
+        # Traite les labels
+        self.labels = np.where(self.labels == 2, self.labels, 0)
+    

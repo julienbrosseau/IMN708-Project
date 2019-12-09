74c84b80f438282dacf787e5f5eaa528cff2cf89
julienbrosseau <jbrosseau85@gmail.com>
2019-12-09 18:06:00 -0500

    MàJ tests et correctifs de la classe "SegSnake"

---
+++ bin/seg_snake.py
@@ -0,0 +1,44 @@
+# Segmentation des images pas snake
+
+import numpy as np
+import skimage.draw as draw
+import skimage.segmentation as seg
+
+class SegSnake():
+
+    def __init__(self):
+        # Initialisation de la classe
+        self.points = []
+        self.labels = []
+
+    def set_circle_points(self, resolution, center, radius):    
+        # Fixe un cercle d'interet sur l'image avec son ensemble de points
+        radians = np.linspace(0, 2*np.pi, resolution)    
+        
+        c = center[1] + radius*np.cos(radians)
+        r = center[0] + radius*np.sin(radians)
+        
+        self.points = np.array([c, r]).T
+    
+    def get_points(self):
+        # Retourne les points du cercle
+        return self.points
+
+    def get_active_contour(self, img_2d, alpha=0.055, beta=0):
+        # Retourne la segmentation de l'image
+        return seg.active_contour(img_2d, self.points, alpha=alpha, beta=beta)
+    
+    def set_labels(self, img_2d, center, radius):
+        # Fixe les labels de l'image
+        labels = np.zeros(img_2d.shape, dtype=np.uint8) 
+
+        indices = draw.circle_perimeter(center[0], center[1], radius)
+
+        labels[indices] = 1
+        labels[self.points[:, 1].astype(np.int), self.points[:, 0].astype(np.int)] = 2
+
+        self.labels = labels
+    
+    def get_seg(self, img_2d, beta):
+        # Retroune la segmentation de l'image
+        return seg.random_walker(img_2d, self.labels, beta=beta)
---
+++ tests/test_seg_snake.py
@@ -0,0 +1,54 @@
+# Test du fichier "seg_snake.py"
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import bin.treatment as tr
+import bin.seg_snake as seg
+
+path             = "data/nifti"
+analyse_axial    = "14971938_T2_AX_FS_20110309130206_8.nii"
+analyse_coronal  = "14971938_T2_STIR_CORO_20110309130206_7.nii"
+analyse_sagittal = "14971938_T1_TSE_SAG_FS_GADO_20110309130206_13.nii"
+
+# Initalisation de la classe "Treatment"
+treatment = tr.Treatment()
+
+# Ouverture du fichier
+img_3d = treatment.open_file(path, analyse_axial)
+
+# Recuperation d'une tranche
+img_2d = treatment.get_slice(img_3d, "axial", 15)
+img_2d = treatment.set_box(img_2d, 150, 150)
+
+# Initialisation de la classe "SegSnake"
+snake = seg.SegSnake()
+
+# Définir le cercle de points
+snake.set_circle_points(200, [75, 75], 34)
+
+# Contour de l'image
+img_contour = snake.get_active_contour(img_2d, 0.05, 0)
+img_cercle = snake.get_points()
+
+# Fixe les labels de l'image
+snake.set_labels(img_2d, [75, 75], 17)
+
+# Segmentation de l'image
+img_seg = snake.get_seg(img_2d, 5000)
+
+# Affichage
+fig, ax = plt.subplots(ncols=3)
+
+ax[0].title.set_text("Image d'origine")
+ax[0].imshow(img_2d, cmap='gray')
+
+ax[1].title.set_text("Contour de l'image")
+ax[1].imshow(img_2d, cmap='gray')
+ax[1].plot(img_cercle[:, 0], img_cercle[:, 1], '--r', lw=1)
+ax[1].plot(img_contour[:, 0], img_contour[:, 1], '-b', lw=1)
+
+ax[2].title.set_text("Post-traitement")
+ax[2].imshow(img_seg, cmap='gray')
+
+plt.show()
